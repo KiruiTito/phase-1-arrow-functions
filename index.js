bf69c76e97469b35221f5ea1f9249a19b912680a
KiruiTito <titokirui402@gmail.com>
2023-03-30 15:17:29 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression for divide
+const divide = function(num1, num2) {
+    return num1 / num2;
+  };
+  
+  // Arrow function for square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Arrow function for add
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
